e1764510f8c83581c08b9f87d681cffe1b01d7c7
Chen Chen <cchenlp@126.com>
2022-12-08 12:21:47 +0800

    Improve dpdk files with Andreas sample

--- Networking/SR-IOV/files/dpdk/entrypoint.sh
+++ Networking/SR-IOV/files/dpdk/entrypoint.sh
@@ -1,4 +1,4 @@
-PCI=$PCIDEVICE_OPENSHIFT_IO_SRIOV_DPDK_ENS4F0
-MAC=`cat /root/mapping.txt | grep $PCI | awk '{print $3}'`
-( echo 'start' ; while true ; do echo 'show port stats all' ; sleep 60 ; done ) | dpdk-testpmd -n 4 -l `cat /sys/fs/cgroup/cpuset/cpuset.cpus` -a $PCIDEVICE_OPENSHIFT_IO_SRIOV_DPDK_ENS4F0 --socket-mem 1024 --vdev=virtio_user0,path=/dev/vhost-net,mac=$MAC -- -i
+PCI=$(env | grep PCI | cut -d= -f2)
+MAC=$(echo "macaddr 0" | /dpdk-ethtool -a $PCI | awk '/Port 0 MAC Address/ {print $NF}')
+( echo 'start' ; while true ; do echo 'show port stats all' ; sleep 60 ; done ) | /dpdk-testpmd -n 4 -l `cat /sys/fs/cgroup/cpuset/cpuset.cpus` -a $PCI --socket-mem 1024 --vdev=virtio_user0,path=/dev/vhost-net,mac=$MAC -- -i
 sleep infinity
